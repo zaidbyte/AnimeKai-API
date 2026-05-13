ac70792d5f8d6283206bd929fbb65bb426afe351
Zaid <191896805+zaidbyte@users.noreply.github.com>
2026-05-12 21:57:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -468,7 +468,9 @@ def api_servers(ep_token):
 @app.route("/api/source/<link_id>", methods=["GET"])
 def api_source(link_id):
     res = resolve_source(link_id)
-    return (jsonify(res), 500) if "error" in res else jsonify({"success": True, **res})
+    if "error" in res:
+        return jsonify(res), 500
+    return jsonify({"success": True, **res})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
